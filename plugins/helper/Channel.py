4148b742ba07621896880cf0d9daa6afbee44b51
Mahi-Sen <155892772+Mahi-Sen@users.noreply.github.com>
2024-11-29 11:41:21 +0530

    Update Channel.py

--- plugins/helper/Channel.py
+++ plugins/helper/Channel.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from pyrogram.types import *
 
 # Replace this with your own channel ID
-CHANNEL_ID = -1002329683513  
+CHANNEL_ID = -1002037872585  
 
 @Client.on_message(filters.channel & filters.media)
 async def add_button(client, message):
